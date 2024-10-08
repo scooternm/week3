d6a83c863a54563fe96538de0c17a461efdfc01c
Kay <mccray382@gmail.com>
2024-10-08 14:24:19 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,4 @@
+a = 3  //declaration
+b = 5
+c = a + b //adding a+b then assigning to c
+console.log(c) //passing c into the function
---
+++ tuesday.js
@@ -0,0 +1,40 @@
+console.log("TUESDAY");
+
+n = 12 //chill
+let x = 123; //formal
+
+i = 123 //integer
+f = 99.99 //float, fraction
+
+num = 1.1 //numbers
+
+text = "123.45 words" 
+number = parseFloat(text)
+double = number * 2
+console.log(double)
+
+d = new Date() //create new date object
+console.log(d)
+ms = d.getTime()
+console.log(ms)
+//fn f5 Run/Execute
+
+let counter = 0
+counter++ //increment 
+counter++
+console.log(counter)
+counter-- //decrement
+console.log(counter)
+
+let level = 0
+level+=5
+level+=5
+level+=5
+console.log(level)
+level -= 5
+console.log(level)
+
+console.log(3 + 5)
+console.log(3 - 5)
+console.log(3 * 5)
+console.log(3 / 5)
