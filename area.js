5316c3d57ed99330ba3caf81cde46920dfec5b80
Kay <mccray382@gmail.com>
2024-10-10 13:14:14 -0400

    functions

---
+++ area.js
@@ -0,0 +1,22 @@
+//area 
+
+//height = 10
+//width = 20
+//area = height * width 
+//console.log(area);
+
+//area is function name
+//width and height are the received parameters/arguments
+function area(width, height){
+    //calculate and send back result to the caller
+    const resultArea = width * height //calculate
+    return resultArea; //send back to caller
+}
+
+console.log(area(12, 12));
+console.log(area(24, 40));
+console.log(area(10, 20));
+height = 10
+width = 20
+myArea = area(width, height)
+console.log(myArea)
